0aa2285a8f4ae308495d6ee815635e234d7a0511
Uncle Cheese <aaron@silverstripe.com>
2015-12-02 14:41:49 +1300

    refactor into two separate extensions, new silverstripe-intercom version

--- code/IntercomUserDefinedFormExtension.php
+++ code/IntercomUserDefinedFormExtension.php
@@ -2,38 +2,13 @@
 
 class IntercomUserDefinedFormExtension extends DataExtension {
 
+
 	/**
 	 * This is a bit of a hack. This is not what this extension point is intended for,
 	 * but we're hijacking it because it's the only extension point that passes the
 	 * $form object.
 	 */
 	public function updateFilteredEmailRecipients ($recipients, $data, $form) {
-		if($this->owner->Fields()->find('MapToIntercom', true)) {
-			$userData = [];
-			$companyData = [];
-			$noteData = [];
-			
-			foreach($this->owner->Fields() as $f) {
-				if(!$f->MapToIntercom) continue;
-				switch($f->IntercomFieldType) {
-					case 'USER':						
-						$userData[$f->Name] = $f->getIntercomFieldName();
-						break;
-					case 'COMPANY':
-						$companyData[$f->Name] = $f->getIntercomFieldName();
-						break;
-					case 'NOTE':
-						$noteData[$f->Name] = $f->NoteLabel;
-						break;
-				}
-
-			}
-
-			$form->setIntercomUserFieldMapping($userData)
-				 ->setIntercomCompanyFieldMapping($companyData)
-				 ->setIntercomNoteMapping($noteData)
-				 ->setIntercomNoteHeader("Submitted on " . date('d-m-Y'))
-				 ->sendToIntercom();
-		}
-	}
+		$form->sendToIntercom();
+	}	
 }
---
+++ code/IntercomUserFormExtension.php
@@ -0,0 +1,38 @@
+<?php
+
+/**
+ * Uses the updateForm() extension point to set the Intercom mapping
+ * on the UserForm object.
+ */
+class IntercomUserFormExtension extends DataExtension {
+
+	public function updateForm () {
+		if(!$this->owner->getController()->Fields()->find('MapToIntercom', true)) return;
+		
+		$userData = [];
+		$companyData = [];
+		$noteData = [];
+		
+		foreach($this->owner->getController()->Fields() as $f) {
+			if(!$f->MapToIntercom) continue;
+
+			switch($f->IntercomFieldType) {
+				case 'USER':						
+					$userData[$f->Name] = $f->getIntercomFieldName();
+					break;
+				case 'COMPANY':
+					$companyData[$f->Name] = $f->getIntercomFieldName();
+					break;
+				case 'NOTE':
+					$noteData[$f->Name] = $f->NoteLabel;
+					break;
+			}
+
+		}
+		
+		$this->owner->setIntercomUserFieldMapping($userData)
+			 		->setIntercomCompanyFieldMapping($companyData)
+			 		->setIntercomNoteMapping($noteData)
+			 		->setIntercomNoteHeader("Submitted on " . date('d-m-Y'));				 		
+	}
+}
